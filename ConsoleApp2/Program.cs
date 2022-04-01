89bef767d55525d4ff035842e1b91f84da1bb009
muradabdullay3v <2uy8uag@code.edu.az>
2022-04-01 19:31:24 +0400

    created

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            User Murad = new User("Murad Abdullazade" , "murad.abdullazade.53@mail.ru");
+            Murad.PasswordChecker("Paroludemirem51355");
+            Murad.Info();
+        }
+    }
+}
---
+++ ConsoleApp2/User.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp2
+{
+    class User: IAccount
+    {
+        public string Fullname { get; set; }
+        public string email { get; set; }
+        public string Password { get; set; }
+        public bool isUpper;
+        public bool isLower;
+        public bool isNumber;
+        public User(string Fullname , string email)
+        {
+            this.Fullname = Fullname;
+            this.email = email;
+        }
+
+        public bool PasswordChecker(string password)
+        {
+            if (password.Length >= 8)
+            {
+                foreach (var item in password)
+                {
+                    if (char.IsUpper((item)))
+                    {
+                        isUpper = true;
+                    }
+                    if (char.IsLower((item)))
+                    {
+                        isLower = true;
+                    }
+                    if (char.IsNumber((item)))
+                    {
+                        isNumber = true;
+                    }
+                }
+                if(isUpper && isLower && isNumber)
+                {
+                    Console.WriteLine("Parol duzgun formatdadir!");
+                    return true;
+                }
+                else
+                {
+                    Console.WriteLine("En azi bir simvol boyukle biri kicikle biri ise reqem olmalidi");
+                    return false;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Shifrenin uzunlugu azdir!");
+                return false;
+            }
+        }
+
+        public void Info()
+        {
+            Console.WriteLine($"Fullname : {Fullname} Email : {email}");
+        }
+
+    }
+}
